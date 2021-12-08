c64f40f5f0fab9ad031ac94b43288b14c3563eae
Valeria Yunga <vayunga@utpl.edu.ec>
2021-12-08 10:59:38 -0500

    primer commit

--- ManejoCicloFor/src/paquete02/Ejemplo01.java
+++ ManejoCicloFor/src/paquete02/Ejemplo01.java
@@ -20,8 +20,13 @@ public class Ejemplo01 {
             
             for (int j = 1; j <= 3; j++) {
                 
-                System.out.printf("%d ", i*j);
-                
+                System.out.printf("%d ", i*j);//1*1=1
+                                              //1*2=2
+                                              //1*3=3
+                                              
+                                              //2*1=2
+                                              //2*2=4
+                                              //2*3=6
                
             }
             System.out.printf("\n-------------\n");
