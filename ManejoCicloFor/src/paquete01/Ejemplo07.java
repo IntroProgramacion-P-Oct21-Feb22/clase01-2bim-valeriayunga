e1ab6d3f8c9ffa81e67bc3cebec15cdbcaa68f85
valeriayunga <vayunga@utpl.edu.ec>
2021-12-08 11:20:15 -0500

    primer commit

--- ManejoCicloFor/src/paquete01/Ejemplo07.java
+++ ManejoCicloFor/src/paquete01/Ejemplo07.java
@@ -12,15 +12,20 @@ package paquete01;
 public class Ejemplo07 {
     public static void main(String[] args) {
         int operacion;
+        String mensajeFinal="";
         
         for (int i = 1; i <= 10; i++) { // for 01
-            System.out.printf("Tabla de multiplicar del número %d\n", i);
+            mensajeFinal=String.format("%sTabla de multiplicar del número"
+                    + " %d\n",mensajeFinal ,i);
             
             for (int contador = 1; contador <= 12; contador++) {
                 operacion = i * contador;
-                System.out.printf("%d x %d = %d\n", i, contador, operacion);
+                mensajeFinal = String.format("%s%d x %d = %d\n",
+                        mensajeFinal , i, contador, operacion);
             }
-            System.out.println("\n");
+            mensajeFinal=String.format ("%s\n",mensajeFinal );
         } // termina for 01
+        
+         System.out.printf("%s\n",mensajeFinal);
     }
 }
--- ManejoCicloFor/src/paquete03/Ejemplo01.java
+++ ManejoCicloFor/src/paquete03/Ejemplo01.java
@@ -15,7 +15,9 @@ public class Ejemplo01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        int i;
+        
+        for ( i = 0; i < 10; i++) {
             System.out.printf("%d", i);
         }
 
